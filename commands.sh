40e737f9e87bda72c4cbc38df1b3f0c2ea22b862
encrypted_jpg <vallakatlaaditya@gmail.com>
2023-02-02 12:17:33 +0530

    Mod commands

--- commands.sh
+++ commands.sh
@@ -1,12 +1,12 @@
 pip install -r requirements.txt
-!pip install --upgrade --no-cache-dir gdown
+pip install --upgrade --no-cache-dir gdown
 # PointNet++
 git clone https://github.com/erikwijmans/Pointnet2_PyTorch.git
 cd Pointnet2_PyTorch/pointnet2_ops_lib
 python setup.py install --user
 cd ../..
 # GPU kNN
-!pip install --upgrade https://github.com/unlimblue/KNN_CUDA/releases/download/0.2/KNN_CUDA-0.2-py3-none-any.whl
+pip install --upgrade https://github.com/unlimblue/KNN_CUDA/releases/download/0.2/KNN_CUDA-0.2-py3-none-any.whl
 cd extensions/chamfer_dist
 python setup.py install --user
 cd ../..
